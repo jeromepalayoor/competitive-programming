7e41838df32c3aa61dfd19c0d336f127d6601d34
jeromepalayoor <me@jp-ch.gq>
2023-08-13 14:46:49 +0800

    ok

--- codeforces/practices/rating/1300/451B-Sort-the-Array.cpp
+++ codeforces/practices/rating/1300/451B-Sort-the-Array.cpp
@@ -7,6 +7,7 @@
 #include <array>
 #include <vector>
 #include <algorithm>
+#include <map>
 #define sz(a) ((int)((a).size()))
 #define char unsigned char
 
@@ -17,7 +18,61 @@ typedef long double ld;
 
 int solve() {
 	int n=0;cin>>n;
-	
+	int a[n],b[n];
+	for (int i = 0; i < n; ++i)
+	{
+		cin>>a[i];
+		b[i]=a[i];
+	}
+	map<int, int> mp;
+	sort(b,b+n);
+	for (int i = 0; i < n; ++i)
+	{
+		mp[b[i]]=i;
+	}
+	for (int i = 0; i < n; ++i)
+	{
+		a[i]=mp[a[i]];
+	}
+	int l=-1,r=-1;
+	for (int i = 0; i < n; ++i)
+	{
+		if (a[i]!=i)
+		{
+			l=i;
+			break;
+		}
+	}
+	for (int i = n-1; i >= 0; --i)
+	{
+		if (a[i]!=i)
+		{
+			r=i;
+			break;
+		}
+	}
+	if (l==-1||r==-1)
+	{
+		cout<<"yes\n1 1"<<endl;
+		return 0;
+	}
+	reverse(a+l,a+r+1);
+	bool sorted=true;
+	for (int i = 0; i < n; ++i)
+	{
+		if (a[i]!=i)
+		{
+			sorted=false;
+		}
+	}
+	if (sorted)
+	{
+		cout<<"yes\n"<<l+1<<" "<<r+1<<endl;
+	}
+	else
+	{
+		cout<<"no"<<endl;
+	}
 	return 0;
 }
 
