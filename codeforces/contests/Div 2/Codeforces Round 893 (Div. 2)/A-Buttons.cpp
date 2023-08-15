bd251cc2122b96b65f87d1f2c734ce65944902c8
jeromepalayoor <me@jp-ch.gq>
2023-08-16 02:11:54 +0800

    contest

---
+++ codeforces/contests/Div 2/Codeforces Round 893 (Div. 2)/A-Buttons.cpp
@@ -0,0 +1,68 @@
+// https://codeforces.com/contest/1858/problem/A
+
+#include <cstdio>
+#include <iostream>
+#include <cstdint>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <ctype.h>
+#include <assert.h>
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <list>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <bitset>
+#include <climits>
+
+#define sz(a) ((int)((a).size()))
+#define char unsigned char
+#define mp make_pair
+#define pb push_back
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+int solve() {
+	int a=0,b=0,c=0;
+	cin>>a>>b>>c;
+	c=c%2;
+	if (c==0)
+	{
+		if (a>b)
+		{
+			cout<<"First"<<endl;
+		}
+		else
+			cout<<"Second"<<endl;
+	}
+	else
+	{
+		b-=1;
+		if (a>b)
+		{
+			cout<<"First"<<endl;
+		}
+		else
+			cout<<"Second"<<endl;	}
+	return 0;
+}
+
+int32_t main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int TET = 1e9;
+	cin >> TET;
+	for (int i = 1; i <= TET; i++) {
+		if (solve()) {
+			break;
+		}
+	}
+}
